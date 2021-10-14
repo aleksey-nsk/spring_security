7e796866439780c7fc598fa71cb6565e0f65716d
Alex Zhd <my_email@test.com>
2021-10-14 23:57:34 +0700

    настроил in memory аутентификацию в коде

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 #### Модуль example1_in_memory
 - In-Memory аутентификация
+
 URL = localhost:8081/api/v1/hello
 Происходит перенаправление на страницу localhost:8081/login
 (страница с формой логина/пароля).
@@ -17,3 +18,17 @@ localhost:8081/logout
 
 Задать своего пользователя в In-Memory аутентификации можно
 в файле application.yaml
+
+Для настройки In-Memory аутентификации в коде необходимо создать 
+класс, который расширяет класс **WebSecurityConfigurerAdapter**,
+и сделать его бином с помощью **@EnableWebSecurity**.
+
+Затем надо переопределить метод **configure(AuthenticationManagerBuilder auth)** 
+класса WebSecurityConfigurerAdapter, и уже с помощью билдера
+AuthenticationManagerBuilder настроить AuthenticationManager (который
+выполняет аутентификацию).
+
+В бине PasswordEncoder задаётся, как шифровать пароль. 
+Мы задали NoOpPasswordEncoder, который не делает ничего — оставляет пароль 
+в первоначальном виде. Конечно, в реальном приложении NoOpPasswordEncoder 
+не пригоден — пароль нужно шифровать, например, с помощью BCryptPasswordEncoder.
---
+++ example1_in_memory/src/main/java/com/spring/example1/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.spring.example1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("u1")
+                    .password("p1")
+                    .authorities("ROLE_USER")
+                .and()
+                .withUser("u2")
+                    .password("p2")
+                    .authorities("ROLE_USER");
+    }
+}
